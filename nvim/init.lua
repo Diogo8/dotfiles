d31e4db8da45c96b107049f43e1ba454572d30a6
Diogo <ddiogo@protonmail.com>
2021-11-14 16:20:55 +0000

    Add nvim lua config

---
+++ nvim/init.lua
@@ -0,0 +1,261 @@
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+local keymap = vim.api.nvim_set_keymap
+keymap('n', '<c-s>', ':w<CR>', {}) 
+local opts = { noremap = true }
+keymap('n', '<c-j>', '<c-w>j', opts) 
+keymap('n', '<c-h>', '<c-w>h', opts) 
+keymap('n', '<c-k>', '<c-w>k', opts) 
+keymap('n', '<c-l>', '<c-w>l', opts) 
+
+keymap('n', '<c-b>', ':NvimTreeToggle<CR>', {})
+keymap('n', '<r>', ':NvimTreeRefresh<CR>', {})
+keymap('n', '<n>', ':NvimTreeFindFile<CR>', {})
+
+keymap('n', '<Leader>f', ':Format', {noremap = true, silent = true})
+
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'tamelion/neovim-molokai'
+  use 'vimwiki/vimwiki'
+  use 'lucastrvsn/kikwis'
+  use 'terrortylor/nvim-comment'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'mhartington/formatter.nvim'
+  use 'hrsh7th/nvim-compe'
+  use 'windwp/nvim-autopairs'
+  use 'glepnir/lspsaga.nvim'
+  use 'neovim/nvim-lspconfig'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
+  }
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use 'phaazon/hop.nvim'
+end)
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.guicursor = {
+  "n-v-c:block",
+  "i-ci-ve:ver25",
+  "r-cr:hor20",
+  "o:hor50",
+  "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+  "sm:block-blinkwait175-blinkoff150-blinkon175",
+}
+
+vim.g.kikwis_pallete = "lopsa"
+
+vim.cmd "colorscheme kikwis"
+
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  resolve_timeout = 800;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+  };
+}
+
+require("nvim-autopairs").setup({})
+require("nvim-autopairs.completion.compe").setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+  auto_select = true, -- auto select first item
+})
+
+
+local saga = require("lspsaga")
+saga.init_lsp_saga({
+  code_action_icon = " ",
+  definition_preview_icon = "  ",
+  dianostic_header_icon = "   ",
+  error_sign = " ",
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
+  hint_sign = "⚡",
+  infor_sign = "",
+  warn_sign = "",
+})
+
+require'lspconfig'.solargraph.setup{}
+
+
+require'nvim-tree'.setup {
+  -- disables netrw completely
+  disable_netrw       = true,
+  -- hijack netrw window on startup
+  hijack_netrw        = true,
+  -- open the tree when running this setup function
+  open_on_setup       = true,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup  = {},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close          = true,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab         = true,
+  -- hijacks new directory buffers when they are opened.
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor       = false,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+  update_cwd          = false,
+  -- show lsp diagnostics in the signcolumn
+  lsp_diagnostics     = false,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable      = false,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd  = nil,
+    -- the command arguments as a list
+    args = {}
+  },
+
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`
+    width = 30,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'right',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = {}
+    }
+  }
+}
+
+require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- DevIcon will be appended to `name`
+ override = {
+  zsh = {
+    icon = "",
+    color = "#428850",
+    name = "Zsh"
+  }
+ };
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+}
+
+require('formatter').setup({
+  filetype = {
+    javascript = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
+          stdin = true
+        }
+      end
+    },
+  }
+})
+
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    theme = "tokyonight",
+    component_separators = { " ", " " },
+    section_separators = { "", "" },
+    disabled_filetypes = {},
+  },
+  sections = {
+    lualine_a = { "mode", "paste" },
+    lualine_b = {
+      { "branch", icon = "" },
+      { "diff", color_added = "#a7c080", color_modified = "#ffdf1b", color_removed = "#ff6666" },
+    },
+    lualine_c = {
+      { "diagnostics", sources = { "nvim_lsp" } },
+      function()
+        return "%="
+      end,
+      "filename",
+      { getWords },
+    },
+    lualine_x = { "filetype" },
+    lualine_y = {
+      {
+        "progress",
+      },
+    },
+    lualine_z = {
+      {
+        "location",
+        icon = "",
+      },
+    },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
+})
